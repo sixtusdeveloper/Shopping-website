f33c61ab8c431aceb160de98cba5d9a6fdd7bbc0
sixtusdev <sixtusushahemba@gmail.com>
2023-02-05 12:32:42 +0100

    commit on auto link indication added

--- js/main.js
+++ js/main.js
@@ -77,3 +77,30 @@ function color() {
   this.classList.add('active')
 }
 colors.forEach((c) => c.addEventListener('click', color))
+
+// Auto indicate active section from the section link script
+const sections = document.querySelectorAll('section')
+const navLinks = document.querySelectorAll('.menu .nav-item a')
+
+const options = {
+  rootMargin: '-50% 0% -50% 0%',
+  threshold: 0,
+}
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const id = entry.target.getAttribute('id')
+      navLinks.forEach((link) => {
+        link.classList.remove('active_links')
+        if (link.getAttribute('href') === `#${id}`) {
+          link.classList.add('active_links')
+        }
+      })
+    }
+  })
+}, options)
+
+sections.forEach((section) => {
+  observer.observe(section)
+})
